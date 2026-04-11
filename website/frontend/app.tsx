d44254439a757fe4c9216d0a6c130666b083b006
Patrick Arminio <patrick.arminio@gmail.com>
2026-04-11 12:03:19 +0100

    Remove website API docs section

--- website/frontend/app.tsx
+++ website/frontend/app.tsx
@@ -1,11 +1,10 @@
-import { APIPage, createDocsApp, DocsPage, HomePage } from '@usecross/docs'
+import { createDocsApp, DocsPage, HomePage } from '@usecross/docs'
 import './styles.css'
 
 createDocsApp({
   pages: {
     'docs/DocsPage': DocsPage,
     'HomePage': HomePage,
-    'api/APIPage': APIPage,
   },
   title: (title) => `${title} - Cross Web`,
 })
--- website/frontend/ssr.tsx
+++ website/frontend/ssr.tsx
@@ -1,11 +1,10 @@
-import { APIPage, DocsPage, HomePage } from '@usecross/docs'
+import { DocsPage, HomePage } from '@usecross/docs'
 import { createDocsServer } from '@usecross/docs/ssr'
 
 createDocsServer({
   pages: {
     'docs/DocsPage': DocsPage,
     'HomePage': HomePage,
-    'api/APIPage': APIPage,
   },
   title: (title) => `${title} - Cross Web`,
 })
